91d317d26828d45e0042810a83150e98ac3a8a88
ixguza <rtk0253@gmail.com>
2015-09-22 02:36:44 +0900

    modalだめ

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -1,5 +1,6 @@
 var svgModalId = "svg_modal";
 var svgModal = documentHere(function(){/*
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all" />
 <div id="***" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="svgModalLabel" style="display: none;">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -17,6 +18,18 @@ var svgModal = documentHere(function(){/*
 */
 }).replace("***", svgModalId);
 
+var svgModal = documentHere(function(){/*
+<section class="modal-window" id="***">
+<div class="modal-inner">
+コンテンツ
+</div>
+<div class="modal-close">&times;</div>
+</section>
+
+*/
+}).replace("***", svgModalId);
+
+
 // bodyにsvgModal追加
 d3.select("body").append("div").html(svgModal);
 
@@ -30,6 +43,12 @@ onLoadNicoPlayer(function(np){
         console.log(c.vpos + "," + c.message);
     });
 
+    // TODO 広告
+    if(true){
+        _.each([/*"#playerTabContainer .playerTabAds", */"#playerTabContainer .nicoSpotAds"], function(a){
+            d3.select(a).style("display", "none");
+        });
+    }
 });
 
 // onClick...
@@ -37,10 +56,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.pageButtonClicked){
         onLoadNicoPlayer(function(np){
 
-            $("#"+svgModalId).modal();
+            //$("#"+svgModalId).modal();
             var comments = np.getComments(1000);
 
-            d3.select("#" + svgModalId).select(".modal-body").append("div")
+            d3.select("#" + svgModalId)
+                .style("display", "block")
+                .select(".modal-inner").append("div")
                 .html(
                     _.chain(comments).first(10).map(function(c){return c.message}).join("<br>")
                 );
