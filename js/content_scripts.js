1d818da7167a600c870e442135f7c9114ab53bc0
ixguza <rtk0253@gmail.com>
2015-09-22 02:48:05 +0900

    だめ

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -25,7 +25,15 @@ var svgModal = documentHere(function(){/*
 </div>
 <div class="modal-close">&times;</div>
 </section>
+*/
+}).replace("***", svgModalId);
 
+var svgModal = documentHere(function(){/*
+<div id="***">
+<div id="modal-content">
+	<p>「閉じる」か「背景」をクリックするとモーダルウィンドウを終了します。</p>
+	<p><a id="modal-close" class="button-link">閉じる</a></p>
+</div></div>
 */
 }).replace("***", svgModalId);
 
@@ -56,6 +64,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.pageButtonClicked){
         onLoadNicoPlayer(function(np){
 
+            //$(this).blur() ;	//ボタンからフォーカスを外す
+            // TODO ↓
+            //if($("#modal-overlay")[0]) return false ;		//新しくモーダルウィンドウを起動しない [下とどちらか選択]
+            //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する [上とどちらか選択]
+
+            //オーバーレイ用のHTMLコードを、[body]内の最後に生成する
+            $("body").append('<div id="modal-overlay"></div>');
+
+            //[$modal-overlay]をフェードインさせる
+            $("#modal-overlay").fadeIn("slow");
+
+            //[$modal-content]をフェードインさせる
+            $("#modal-content").fadeIn("slow");
+
+            centeringModalSyncer();
+
             //$("#"+svgModalId).modal();
             var comments = np.getComments(1000);
 
@@ -70,3 +94,32 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         sendResponse("ok");
     };
 });
+
+//センタリングをする関数
+function centeringModalSyncer(){
+
+	//画面(ウィンドウ)の幅を取得し、変数[w]に格納
+	var w = $(window).width();
+
+	//画面(ウィンドウ)の高さを取得し、変数[h]に格納
+	var h = $(window).height();
+
+	//コンテンツ(#modal-content)の幅を取得し、変数[cw]に格納
+	var cw = $("#modal-content").outerWidth({margin:true});
+
+	//コンテンツ(#modal-content)の高さを取得し、変数[ch]に格納
+	var ch = $("#modal-content").outerHeight({margin:true});
+
+	//コンテンツ(#modal-content)を真ん中に配置するのに、左端から何ピクセル離せばいいか？を計算して、変数[pxleft]に格納
+	var pxleft = ((w - cw)/2);
+
+	//コンテンツ(#modal-content)を真ん中に配置するのに、上部から何ピクセル離せばいいか？を計算して、変数[pxtop]に格納
+	var pxtop = ((h - ch)/2);
+
+	//[#modal-content]のCSSに[left]の値(pxleft)を設定
+	$("#modal-content").css({"left": pxleft + "px"});
+
+	//[#modal-content]のCSSに[top]の値(pxtop)を設定
+	$("#modal-content").css({"top": pxtop + "px"});
+
+}
